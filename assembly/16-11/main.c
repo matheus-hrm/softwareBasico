c82d048f1f4faefc960b6b9bfde598f4bd47512a
Matheus Henrique <rodriguesm673@gmail.com>
2023-11-16 11:45:55 -0300

    first assembly class

---
+++ assembly/16-11/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Indica que as variáveis existem e são definidas em outro lugar
+
+extern char c;
+
+extern int i;
+
+extern int j;
+
+extern long l;
+
+extern unsigned short us;
+
+extern unsigned int ui;
+
+extern unsigned long ul;
+
+void sec();
+
+int main()
+
+{
+
+    sec();
+
+    printf("c  = %d\n", c);
+
+    printf("i  = %d\n", i);
+
+    printf("j  = %d\n", j);
+
+    printf("l  = %ld\n", l);
+
+    printf("us = %u\n", us);
+
+    printf("ui = %u\n", ui);
+
+    printf("lu = %lu\n", ul);
+
+    return 0;
+}
--- floating-point/int2float.c
+++ floating-point/int2float.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 #include <math.h>
 
-
 #define getsig(i)        ((i)>>31 & 1)
-
 #define getexp(i)        ((i)>>23 & 0xff)
-
 #define getfrac(i)       ((i) & 0x7fffff)
-
 #define makefloat(s,e,f) ( (((s) & 1) << 31) | (((e) & 0xff) << 23) | ((f) & 0x7fffff) )
-
 
 typedef union {
 
@@ -54,59 +48,47 @@ float int2float(int i){
   return u.f;
 }
 
-
 int main() {
 
    int i;
 
-
    printf("\n******** int2float ****************\n");
-
 
    i = 0;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
 
-
    i = 1;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
-
 
    i = -1;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
 
-
    i = 12345;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
-
 
    i = -12345;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
 
-
    i = 13631488;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
-
 
    i = 1879048192;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
 
-
    i = 0x7fffffff;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
 
-
    i = -i;
 
    printf(" %d -> %+10.4f, %+10.4f\n", i, (float)i, int2float(i));
-
 
    return 0;
 
