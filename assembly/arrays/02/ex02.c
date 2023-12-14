2a0e72c33cf500e591af707b449bb03fb2f74f23
matheus-hrm <rodriguesm673@gmail.com>
2023-12-14 12:03:34 -0300

    loop fix

--- assembly/arrays/02/ex02.c
+++ assembly/arrays/02/ex02.c
@@ -5,23 +5,13 @@ int even = 0;
 int odd = 0;
 
 
-void ex02()
-
-{
-
+void ex02(){
    unsigned int count = 0;
-
-
+   
    while (letters[count]) {
-
       if (letters[count++] & 1)
-
         odd++;
-
       else
-
         even++;
-
    }
-
 }
